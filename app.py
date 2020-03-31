60228898780bf6a45910a9cf4eabc505d94e74fd
Raja Raman <raja.raman@kwikee.com>
2020-03-31 13:08:20 -0400

    Reverse name updated

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
 # app.py - a minimal flask api using flask_restful
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
+application = Flask(__name__)
+api = Api(application)
 
 class HelloWorld(Resource):
+    
     def get(self):
         return {
-            'hello': 'world',
-            'one' : 'two',
-            'three' : 'four'
+            "name" : "John",
+            "city" : "Toronto",
+            "country" : "Canada"
             }
 
-api.add_resource(HelloWorld, '/')
+# api.add_resource(HelloWorld, '/')
 
-@app.route("/reverse")
+@application.route("/reverse")
 def hello():
 
     name = request.args.get('name', default='Aloha')
+    reverse_name = name[::-1]
+
+    return reverse_name
+
+def reverse_name(name):
 
     reverse_name = name[::-1]
 
     return reverse_name
 
+@application.route('/', methods=['GET', 'POST'])
+def home():
+
+    if(request.method == 'GET'):
+        return render_template('index.html')
+
+    # Post
+    name  = request.form.get('name')
+    
+    r_name = reverse_name(name)
+    
+    result = {
+        'name' : name,
+        'r_name' : r_name
+    }
+    
+    #return content
+    return render_template('index.html', result=result)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    application.run(debug=True, host='0.0.0.0')
